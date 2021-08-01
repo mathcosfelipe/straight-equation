08762d701409d3ece71d3c3a4ac41c322fa11263
marcosfbc <marcosfbdcarvalho@gmail.com>
2021-08-01 14:13:25 -0300

    Finalized

--- src/main.py
+++ src/main.py
@@ -1,8 +1,357 @@
-from languages.english import english
-from languages.spanish import spanish
-from languages.german import german
-from languages.portuguese import portuguese
-from languages.italian import italian
+def generalConstant(xa, ya, xb, yb):
+
+    coeficient = ""
+    
+    # d = |x  y  1|x   y|
+    #     |xa ya 1|xa ya|
+    #     |xb yb 1|xb yb|
+    
+    secondaryC = xb * ya * -1
+    mainC = xa * yb
+    constant = secondaryC + mainC
+    
+    if constant < 0:
+      coeficient = f' - {constant * -1}'
+    elif constant > 0:
+      coeficient =  f' + {constant}'
+    else:
+      coeficient = f''
+      
+    return coeficient
+
+def generalLinear(xa, ya, xb, yb):
+
+    coeficient = ""
+    
+    # d = |x  y  1|x   y|
+    #     |xa ya 1|xa ya|
+    #     |xb yb 1|xb yb|
+    
+    secondaryY = xa * -1
+    mainY = xb
+    linear = secondaryY + mainY
+    
+    if linear < 0:
+      coeficient = f' - {linear * -1}y'
+    elif linear > 0:
+      coeficient = f' + {linear}y'
+    else:
+      coeficient = f''
+      
+    return coeficient
+
+def generalAngular(xa, ya, xb, yb):
+
+    coeficient = ""
+    
+    # d = |x  y  1|x   y|
+    #     |xa ya 1|xa ya|
+    #     |xb yb 1|xb yb|
+    
+    secondaryX = yb * -1
+    mainX = ya
+    angular = secondaryX + mainX
+    
+    if angular < 0:
+      coeficient = f'- {angular * -1}x'
+    elif angular > 0:
+      coeficient = f'{angular}x'
+    else:
+      coeficient = ""
+      
+    return coeficient
+
+def reducedLinear(xa, ya, xb, yb):
+
+    coeficient = ""
+    
+    # y - y0 = m(x - x0)
+   
+    ybya = yb - ya
+    xbxa = xb - xa
+    angular = ybya / xbxa
+    linear = angular * xa + ya
+    
+    if linear < 0:
+      coeficient = f' - {linear * -1}'
+    elif linear > 0:
+      coeficient = f' + {linear}'
+    else:
+      coeficient = ""
+
+    return coeficient
+
+def reducedAngular(xa, ya, xb, yb):
+
+    coeficient = ""
+   
+    ybya = yb - ya
+    xbxa = xb - xa
+    angular = ybya / xbxa
+    
+    if angular < 0:
+      coeficient = f'- {angular * -1}x'
+    elif angular > 0:
+      coeficient =  f'+ {angular}x'
+    else:
+      coeficient = ""
+
+    return coeficient
+
+def spanish():
+    
+    while True:
+        try:
+            xa = float(input("Ingrese el valor de xa: "))
+        except:
+            print("¡Error! Valor no válido. Escriba otra vez.")
+        else:
+            break
+
+    while True:
+        try:
+            ya = float(input("Ingrese el valor de ya: "))
+        except:
+            print("¡Error! Valor no válido. Escriba otra vez.")
+        else:
+            break
+
+    while True:
+        try:
+            xb = float(input("Ingrese el valor de xb: "))
+        except:
+            print("¡Error! Valor no válido. Escriba otra vez.")
+        else:
+            break
+
+    while True:
+        try:
+            yb = float(input("Ingrese el valor de yb: "))
+        except:
+            print("¡Error! Valor no válido. Escriba otra vez.")
+        else:
+            break
+
+    reduced = 'y = ' + reducedAngular(xa, ya, xb, yb) + reducedLinear(xa, ya, xb, yb)
+    general = generalAngular(xa, ya, xb, yb) + generalLinear(xa, ya, xb, yb) + generalConstant(xa, ya, xb, yb)
+    answer = f'\n La ecuación reducida es: {reduced} \n La ecuación general es: {general}'
+
+    print(answer)
+
+    decision = input("¿Quieres calcular otra ecuación (s/n)?")
+
+    while decision != "s" and decision != "n":
+        print("¡Error! Opción inválida. Escriba otra vez.")
+        decision = input("¿Quieres calcular otra ecuación (s/n)?")
+
+
+    if decision == "s":
+        spanish()
+    else:
+        quit()
+
+def portuguese():
+    
+    while True:
+        try:
+            xa = float(input("Informe o valor de xa: "))
+        except:
+            print("Erro! Valor inválido. Digite novamente.")
+        else:
+            break
+
+    while True:
+        try:
+            ya = float(input("Informe o valor de ya: "))
+        except:
+            print("Erro! Valor inválido. Digite novamente.")
+        else:
+            break
+
+    while True:
+        try:
+            xb = float(input("Informe o valor de xb: "))
+        except:
+            print("Erro! Valor inválido. Digite novamente.")
+        else:
+            break
+
+    while True:
+        try:
+            yb = float(input("Informe o valor de yb: "))
+        except:
+            print("Erro! Valor inválido. Digite novamente.")
+        else:
+            break
+
+    reduced = 'y = '+ reducedAngular(xa, ya, xb, yb) + reducedLinear(xa, ya, xb, yb)
+    general = generalAngular(xa, ya, xb, yb) + generalLinear(xa, ya, xb, yb) + generalConstant(xa, ya, xb, yb)
+    answer = f'\n A equação reduzida é: {reduced} \n A equação geral é: {general}'
+
+    print(answer)
+
+    decision = input("Deseja calcular outra equação (s/n)? ")
+
+    while decision != "s" and decision != "n":
+        print("Erro! Opção inválida. Digite novamente.")
+        decision = input("Deseja calcular outra equação (s/n)? ")
+
+    if decision == "s":
+        portuguese()
+    else:
+        quit()
+
+def italian():
+    
+    while True:
+        try:
+            xa = float(input("Inserisci il valore di xa: "))
+        except:
+            print("Errore! Valore non valido. Digita di nuovo.")
+        else:
+            break
+
+    while True:
+        try:
+            ya = float(input("Inserisci il valore di ya: "))
+        except:
+            print("Errore! Valore non valido. Digita di nuovo.")
+        else:
+            break
+
+    while True:
+        try:
+            xb = float(input("Inserisci il valore di xb: "))
+        except:
+            print("Errore! Valore non valido. Digita di nuovo.")
+        else:
+            break
+
+    while True:
+        try:
+            yb = float(input("Inserisci il valore di yb: "))
+        except:
+            print("Errore! Valore non valido. Digita di nuovo.")
+        else:
+            break
+
+    reduced = 'y = ' + reducedAngular(xa, ya, xb, yb) + reducedLinear(xa, ya, xb, yb)
+    general = generalAngular(xa, ya, xb, yb) + generalLinear(xa, ya, xb, yb) + generalConstant(xa, ya, xb, yb)
+    answer = f"\n L'equazione ridotta è: {reduced} \n L'equazione generale è: {general}"
+
+    print(answer)
+
+    decision = input("Vuoi calcolare un'altra equazione (s/n)?")
+
+    while decision != "s" and decision != "n":
+        print("Errore! Opzione non valida. Digita di nuovo.")
+        decision = input("Vuoi calcolare un'altra equazione (s/n)?")
+
+    if decision == "s":
+        italian()
+    else:
+        quit()
+
+def german():
+    
+    while True:
+        try:
+            xa = float(input("Geben Sie den Wert von ein xa: "))
+        except:
+            print("Fehler! Ungültiger Wert. Tippe nochmal.")
+        else:
+            break
+
+    while True:
+        try:
+            ya = float(input("Geben Sie den Wert von ein ya: "))
+        except:
+            print("Fehler! Ungültiger Wert. Tippe nochmal.")
+        else:
+            break
+
+    while True:
+        try:
+            xb = float(input("Geben Sie den Wert von ein xb: "))
+        except:
+            print("Fehler! Ungültiger Wert. Tippe nochmal.")
+        else:
+            break
+
+    while True:
+        try:
+            yb = float(input("Geben Sie den Wert von ein yb: "))
+        except:
+            print("Fehler! Ungültiger Wert. Tippe nochmal.")
+        else:
+            break
+
+    reduced = 'y = ' + reducedAngular(xa, ya, xb, yb) + reducedLinear(xa, ya, xb, yb)
+    general = generalAngular(xa, ya, xb, yb) + generalLinear(xa, ya, xb, yb) + generalConstant(xa, ya, xb, yb)
+    answer = f'\n Die reduzierte Gleichung lautet: {reduced} \n Die allgemeine Gleichung lautet: {general}'
+
+    print(answer)
+
+    decision = input("Möchten Sie eine andere Gleichung (j/n) berechnen?")
+
+    while decision != "j" and decision != "n":
+        print("Fehler! Ungültige Option. Tippe nochmal.")
+        decision = input("Möchten Sie eine andere Gleichung (j/n) berechnen?")
+
+    if decision == "j":
+        german()
+    else:
+        quit()
+
+def english():
+    
+    while True:
+        try:
+            xa = float(input("Inform the xa value: "))
+        except:
+            print("Error! Invalid value. Try again.")
+        else:
+            break
+
+    while True:
+        try:
+            ya = float(input("Inform the ya value: "))
+        except:
+            print("Error! Invalid value. Try again.")
+        else:
+            break
+
+    while True:
+        try:
+            xb = float(input("Inform the xb value: "))
+        except:
+            print("Error! Invalid value. Try again.")
+        else:
+            break
+
+    while True:
+        try:
+            yb = float(input("Informe o valor de yb: "))
+        except:
+            print("Error! Invalid value. Try again.")
+        else:
+            break
+
+    reduced = 'y = ' + reducedAngular(xa, ya, xb, yb) + reducedLinear(xa, ya, xb, yb)
+    general = generalAngular(xa, ya, xb, yb) + generalLinear(xa, ya, xb, yb) + generalConstant(xa, ya, xb, yb)
+    answer = f'\n The reduced equation is: {reduced} \n The general equation is: {general}'
+
+    print(answer)
+
+    decision = input("Do you wanna calculate another equation (y/n)? ")
+
+    while decision != "y" and decision != "n":
+        print("Error! Invalid option. Try again.")
+
+    if decision == "y":
+        english()
+    else:
+        quit()
 
 def main():
     while True:
