e58bc790ac141df1174ff015909737ec9676daba
Marcos Felipe <78696211+marcosfbc@users.noreply.github.com>
2021-08-01 10:50:31 -0300

    Update linear.py

--- src/reduced/linear.py
+++ src/reduced/linear.py
@@ -1,5 +1,20 @@
-def reducedLinear():
 
-    equation = ""
+def reducedLinear(xa, ya, xb, yb):
 
-    return equation
+    coeficient = ""
+    
+    # y - y0 = m(x - x0)
+   
+    ybya = yb - ya
+    xbxa = xb - xa
+    angular = ybya / xbxa
+    linear = angular * xa + ya
+    
+    if linear < 0:
+      coeficient = " - ", linear
+    elif linear > 0:
+      coeficient = " + ", linear
+    else:
+      coeficient = ""
+
+    return coeficient
