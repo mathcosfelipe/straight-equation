930bf3fbfcfb7e2145ebf4f8794f7cdea0e71452
Marcos Felipe <78696211+marcosfbc@users.noreply.github.com>
2021-08-01 11:29:50 -0300

    Update angular.py

--- src/general/angular.py
+++ src/general/angular.py
@@ -1,5 +1,20 @@
-def generalAngular():
+def generalAngular(xa, ya, xb, yb):
 
-    equation = ""
-
-    return equation
+    coeficient = ""
+    
+    # d = |x  y  1|x   y|
+    #     |xa ya 1|xa ya|
+    #     |xb yb 1|xb yb|
+    
+    secondaryX = yb * -1
+    mainX = ya
+    angular = secondaryX + mainX
+    
+    if angular < 0:
+      coeficient = "- ", angular * -1, "x"
+    elif angular > 0:
+      coeficient = " + ", angular, "x"
+    else:
+      coeficient = ""
+      
+    return coeficient
