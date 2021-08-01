26a6efa781a01cbc5c272d3789653e697b036ae9
Marcos Felipe <78696211+marcosfbc@users.noreply.github.com>
2021-08-01 11:31:12 -0300

    Update linear.py

--- src/general/linear.py
+++ src/general/linear.py
@@ -1,5 +1,20 @@
 def generalLinear():
 
-    equation = ""
-
-    return equation
+    coeficient = ""
+    
+    # d = |x  y  1|x   y|
+    #     |xa ya 1|xa ya|
+    #     |xb yb 1|xb yb|
+    
+    secondaryY = xa * -1
+    mainY = xb
+    linear = secondaryY + mainY
+    
+    if linear < 0:
+      coeficient = " - ", linear * -1, "y"
+    elif linear > 0:
+      coeficient = " + ", linear, "y"
+    else:
+      coeficient = ""
+      
+    return coeficient
