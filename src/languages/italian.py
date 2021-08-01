aa96fb3aed919d92db6643d92d824cc791d670ac
marcosfbc <marcosfbdcarvalho@gmail.com>
2021-07-31 23:56:43 -0300

    Update italian.py

---
+++ src/languages/italian.py
@@ -0,0 +1,55 @@
+from ..reduced.angular import main as ra
+from ..reduced.linear import main as rl
+from ..general.angular import main as ga
+from ..general.linear import main as gl
+from ..general.constant import main as gc
+
+def italian():
+    
+    while True:
+        try:
+            xa = float(input("Inserisci il valore di xa: "))
+        except:
+            print("Errore! Valore non valido. Digita di nuovo.")
+        else:
+            break
+
+    while True:
+        try:
+            ya = float(input("Inserisci il valore di ya: "))
+        except:
+            print("Errore! Valore non valido. Digita di nuovo.")
+        else:
+            break
+
+    while True:
+        try:
+            xb = float(input("Inserisci il valore di xb: "))
+        except:
+            print("Errore! Valore non valido. Digita di nuovo.")
+        else:
+            break
+
+    while True:
+        try:
+            yb = float(input("Inserisci il valore di yb: "))
+        except:
+            print("Errore! Valore non valido. Digita di nuovo.")
+        else:
+            break
+
+    reduced = f'{ra(xa, ya, xb, yb)}{rl(xa, ya, xb, yb)}'
+    general = f'{ga(xa, ya, xb, yb)}{gl(xa, ya, xb, yb)}{gc(xa, ya, xb, yb)}'
+    answer = f"\n L'equazione ridotta è: {reduced} \n L'equazione generale è: {general}"
+
+    print(answer)
+
+    decision = input("Vuoi calcolare un'altra equazione (s/n)?")
+
+    while decision != "s" and decision != "n":
+        print("Errore! Opzione non valida. Digita di nuovo.")
+
+    if decision == "s":
+        italian()
+    else:
+        quit()
