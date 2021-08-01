299cce85328c606a17f408cc0f9bdda63a33897d
Marcos Felipe <78696211+marcosfbc@users.noreply.github.com>
2021-08-01 11:29:08 -0300

    Update constant.py

--- src/general/constant.py
+++ src/general/constant.py
@@ -1,5 +1,20 @@
 def generalConstant():
 
-    equation = ""
-
-    return equation
+    coeficient = ""
+    
+    # d = |x  y  1|x   y|
+    #     |xa ya 1|xa ya|
+    #     |xb yb 1|xb yb|
+    
+    secondaryC = xb * ya * -1
+    mainC = xa * yb
+    constant = secondaryC + mainC
+    
+    if constant < 0:
+      coeficient = "- ", constant * -1
+    elif constant > 0:
+      coeficient = " + ", constant
+    else:
+      coeficient = ""
+      
+    return coeficient
