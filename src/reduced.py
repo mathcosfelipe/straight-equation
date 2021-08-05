47d63892e80e5458d374bbe55bb4c253c04cc9c1
Marcos Felipe <marcosfbdcarvalho@gmail.com>
2021-08-05 20:53:42 -0300

    Create reduced.py

---
+++ src/reduced.py
@@ -0,0 +1,36 @@
+def reducedLinear(xa, ya, xb, yb):
+
+    coeficient = ""
+    
+    # y - y0 = m(x - x0)
+   
+    ybya = yb - ya
+    xbxa = xb - xa
+    angular = ybya / xbxa
+    linear = angular * xa + ya
+    
+    if linear < 0:
+      coeficient = f' - {linear * -1}'
+    elif linear > 0:
+      coeficient = f' + {linear}'
+    else:
+      coeficient = ""
+
+    return coeficient
+
+def reducedAngular(xa, ya, xb, yb):
+
+    coeficient = ""
+   
+    ybya = yb - ya
+    xbxa = xb - xa
+    angular = ybya / xbxa
+    
+    if angular < 0:
+      coeficient = f'- {angular * -1}x'
+    elif angular > 0:
+      coeficient =  f'+ {angular}x'
+    else:
+      coeficient = ""
+
+    return coeficient
