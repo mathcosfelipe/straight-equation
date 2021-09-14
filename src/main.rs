b8c40de600db475b5d0a85ca4ad3835a7fa5fce3
Marcos Felipe <marcosfbdcarvalho@gmail.com>
2021-09-13 23:25:40 -0300

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+fn linear_coeficient_reduced(xa: f64, ya: f64, xb: f64, yb: f64){
+
+}
+
+fn angular_coeficient_reduced(xa: f64, ya: f64, xb: f64, yb: f64){
+    let yb_ya: f64 = yb - ya;
+    let xb_xa: f64 = xb - xa;
+    let angular_coeficient: f64 = yb_ya / xb_xa;
+    
+    if angular_coeficient < 0.0{
+    }else if angular_coeficient > 0.0{
+
+    }else{
+
+    }
+    
+}
+
+fn general_equation(xa: f64, ya: f64, xb: f64, yb: f64){
+
+}
+
+fn reduced_equation(xa: f64, ya: f64, xb: f64, yb: f64){
+    let _angular_coeficient = angular_coeficient_reduced(xa, ya, xb, yb);
+    let _linear_coeficient = linear_coeficient_reduced(xa, ya, xb, yb);
+}
+
+fn main(){
+    let xa: f64 = 5.5;
+    let ya: f64 = 10.5;
+    let xb: f64 = 15.5;
+    let yb: f64 = 20.5;
+    let _reduced_equation = reduced_equation(xa, ya, xb, yb);
+    let _general_equation = general_equation(xa, ya, xb, yb);
+}
