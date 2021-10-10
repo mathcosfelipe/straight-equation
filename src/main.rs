ea0429c9a04a6c74d8f03e6dd360c445531709ab
Marcos Felipe <marcosfbdcarvalho@gmail.com>
2021-10-10 11:05:13 -0300

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,64 @@
-mod reduced_equation;
+use std::io;
 
-use reduced_equation::reduced_equation;
+fn linear_coeficient_reduced(xa:f64, ya:f64, xb:f64, yb:f64) -> String{
+    let ya_yb:f64 = ya - yb;
+    let xa_xb:f64 = xa - xb;
+    let angular_coeficient:f64 = ya_yb / xa_xb;
+    let angular_coeficient_xa:f64 = angular_coeficient * xa;
+    let linear_coeficient:f64 = angular_coeficient_xa + ya;
+    let value:String;
+    if linear_coeficient < 0.0{
+        value = format!(" - {}", (linear_coeficient * -1.0).to_string());
+    }else if linear_coeficient > 0.0{
+        value = format!(" + {}", (linear_coeficient).to_string());
+    }else{
+        value = format!("");
+    }
+    return value;
+}
+
+fn angular_coeficient_reduced(xa:f64, ya:f64, xb:f64, yb:f64) -> String{
+    let ya_yb:f64 = ya - yb;
+    let xa_xb:f64 = xa - xb;
+    let angular_coeficient:f64 = ya_yb / xa_xb;
+    let value:String;
+    if angular_coeficient < 0.0{
+        value = format!(" - {}x", (angular_coeficient * -1.0).to_string());
+    }else if angular_coeficient > 0.0{
+        value = format!(" {}x", (angular_coeficient).to_string());
+    }else{
+        value = format!("");
+    }
+    return value;
+}
+
+fn reduced_equation(xa:f64, ya:f64, xb:f64, yb:f64) -> String{
+    let angular_coeficient:String = angular_coeficient_reduced(xa, ya, xb, yb);
+    let linear_coeficient:String = linear_coeficient_reduced(xa, ya, xb, yb);
+    let reduced_equation: String = (format!("The reduced equation is: {:?}{:?}", angular_coeficient, linear_coeficient)).to_string();
+    return reduced_equation;
+}
 
 fn main(){
-    let xa:f64 = 10.5;
-    let ya:f64 = 20.5;
-    let xb:f64 = 30.5;
-    let yb:f64 = 40.5;
     
-    let reduced_equation:() = reduced_equation::reduced_equation(xa, ya, xb, yb);
-    println!("The reduced equation is {:?}", reduced_equation);
+    let mut xa = String::new();
+    io::stdin().read_line(&mut xa).expect("Failed to read input.");
+    let xa: f64 = xa.trim().parse().expect("Invalid input.");
+    
+    let mut ya = String::new();
+    io::stdin().read_line(&mut ya).expect("Failed to read input.");
+    let ya: f64 = ya.trim().parse().expect("Invalid input.");
+    
+    let mut xb = String::new();
+    io::stdin().read_line(&mut xb).expect("Failed to read input.");
+    let xb: f64 = xb.trim().parse().expect("Invalid input.");
+
+    let mut yb = String::new();
+    io::stdin().read_line(&mut yb).expect("Failed to read input.");
+    let yb: f64 = yb.trim().parse().expect("Invalid input.");
+
+    let reduced_equation:String = reduced_equation(xa, ya, xb, yb);
+
+    println!("{:?}", reduced_equation);
+
 }
--- target/debug/deps/straight_equation-34a8922bbe78907c.d
+++ target/debug/deps/straight_equation-34a8922bbe78907c.d
@@ -1,6 +1,5 @@
-c:\Projetos\StraightEquation\target\debug\deps\straight_equation-34a8922bbe78907c.rmeta: src\main.rs src\reduced_equation.rs
+c:\Projetos\StraightEquation\target\debug\deps\straight_equation-34a8922bbe78907c.rmeta: src\main.rs
 
-c:\Projetos\StraightEquation\target\debug\deps\straight_equation-34a8922bbe78907c.d: src\main.rs src\reduced_equation.rs
+c:\Projetos\StraightEquation\target\debug\deps\straight_equation-34a8922bbe78907c.d: src\main.rs
 
 src\main.rs:
-src\reduced_equation.rs:
