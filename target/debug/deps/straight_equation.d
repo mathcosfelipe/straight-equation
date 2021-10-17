d6873983667dc57a41b360467e706caf8d78f79e
Marcos Felipe <marcosfbdcarvalho@gmail.com>
2021-10-16 22:35:27 -0300

    commit

--- target/debug/deps/straight_equation.d
+++
@@ -1,5 +0,0 @@
-C:\Projetos\StraightEquation\target\debug\deps\straight_equation.exe: src\main.rs
-
-C:\Projetos\StraightEquation\target\debug\deps\straight_equation.d: src\main.rs
-
-src\main.rs:
--- target/debug/straight_equation.d
+++
@@ -1,1 +0,0 @@
-C:\Projetos\StraightEquation\target\debug\straight_equation.exe: C:\Projetos\StraightEquation\src\main.rs
