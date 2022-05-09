a012499283adeac94d80b1dd0b32bba1c8288053
Marcos Felipe <mathcosfelipe@gmail.com>
2022-05-09 12:45:32 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,29 @@ mod straight;
 fn main() {
     
     let xa: f64 = loop {
-        if let Ok(n) = input("Input xa value: ") {
-            break n
+        if let Ok(xa_validation) = input("Input xa value: ") {
+            break xa_validation
         };
         println!("Invalid value! Input a float number. Try again.");
     };
 
     let ya: f64 = loop {
-        if let Ok(n) = input("Input yb value: ") {
-            break n
+        if let Ok(ya_validation) = input("Input yb value: ") {
+            break ya_validation
         };
         println!("Invalid value! Input a float number. Try again.");
     };
 
     let xb: f64 = loop {
-        if let Ok(n) = input("Input yb value: ") {
-            break n
+        if let Ok(xb_validation) = input("Input yb value: ") {
+            break xb_validation
         };
         println!("Invalid value! Input a float number. Try again.");
     };
 
     let yb: f64 = loop {
-        if let Ok(n) = input("Input yb value: ") {
-            break n
+        if let Ok(yb_validation) = input("Input yb value: ") {
+            break yb_validation
         };
         println!("Invalid value! Input a float number. Try again.");
     };
